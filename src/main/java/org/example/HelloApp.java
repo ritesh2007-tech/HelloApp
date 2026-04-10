3c35bc68c33cf3c86536130c048a14297df6037d
ritesh2007-tech <riteshrathanlal30@gmail.com>
2026-04-10 13:34:17 +0530

    use case 7

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -6,20 +6,9 @@ public class HelloApp {
 
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
-
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        String names = "";
-        if (nameBuilder.length() > 0) {
-            names = nameBuilder.substring(0, nameBuilder.length() - 2);
-        }
-
-        System.out.println("Hello, " + names + "!");
     }
 }
